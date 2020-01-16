77d6a77c2fdaae3ff3dd8d8ee614c5140e8820fa
Scott Hoisington <hoyzington@gmail.com>
2020-01-16 09:15:47 +0000

    previous commit was from bin only; re-commited from top

--- Gemfile
+++ Gemfile
@@ -1,4 +1,10 @@
 source "https://rubygems.org"
 
+gem 'require_all'
+gem 'nokogiri'
+gem 'rspec'
+gem 'pry'
+
+
 # Specify your gem's dependencies in nation_info.gemspec
 gemspec
--- lib/nation_info.rb
+++ lib/nation_info.rb
@@ -1,4 +1,11 @@
-require "nation_info/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative './nation_info/version'
+require_relative './nation_info/scraper'
+require_relative './nation_info/nation'
+
 
 module NationInfo
   class Error < StandardError; end
