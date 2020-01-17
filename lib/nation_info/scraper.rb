0c7ccf1ffca2453887a30a5cb7680563e505f747
Scott Hoisington <hoyzington@gmail.com>
2020-01-17 08:40:30 +0000

    finished scraper class

--- lib/nation_info/scraper.rb
+++ lib/nation_info/scraper.rb
@@ -9,45 +9,34 @@ class Scraper
   end
   
   def self.get_nations
+    result = []
     nations = Nokogiri::HTML(open(@base_path + "/geo/en/countries-list/Country-Codes-ISO-3166-ISO-Numeric-ISO3-FIPS-ccTLD.html")).css("tbody tr")
-    nations.each do |n|
+    nations.each_with_index do |n, i|
       data = n.css("a")[1]
-      nation = Nation.new
-      nation.name = data.text
-      nation.url = data.attribute("href").value
+      result[i] = {:name => data.text, :url => data.attribute("href").value}
     end
+    result
   end
   
   def self.nation_info(nation_url)
-    Nokogiri::HTML(open(@base_path + nation_url))
+    data = Nokogiri::HTML(open(@base_path + nation_url))
+    result = {
+      :location => data.css("#Location p").text,
+      :size => data.css("#Area td").children[1].text + " sq km",
+      :climate => data.css("#Climate p").text.capitalize,
+      :population => data.css("#Population p").text,
+      :religions => reformatter(nation_data, "#Religions"),
+      :languages => reformatter(nation_data, "#Languages"),
+      :capital => data.css("#Capital .geo").text,
+      :ppp_per_capita => "$" + data.css("#GDP tbody").children[7].css("td")[1].text
+    }
+    result
   end
     
-  
-end
-
-
-
-nation = list[i].css("a")[1]
-name = nation.text
-url = nation.attribute("href").value
-
-
-afgan = Nokogiri::HTML(open('http://www.geognos.com/geo/en/countries-list/Country-Codes-ISO-3166-ISO-Numeric-ISO3-FIPS-ccTLD.html')).css("tbody tr")[0].css("a")[1]
-
-nation_data = Nokogiri::HTML(open("http://www.geognos.com/geo/en/cc/af.html"))
-
-
-location = nation_data.css("#Location p").text
-size = nation_data.css("#Area td").children[1].text + " sq km"
-climate = nation_data.css("#Climate p").text.capitalize
-population = nation_data.css("#Population p").text
-religions = reformatter(nation_data, "#Religions")
-languages = reformatter(nation_data, "#Languages")
-
-  def reformatter(nation_data, info)
-    data = nation_data.css("#{info} tbody td").children
+  def reformatter(data, info)
+    stats = data.css("#{info} tbody td").children
     string_pieces = []
-    data.each do |d|
+    stats.each do |d|
       string_pieces << d.text
     end
     data_array = []
@@ -60,21 +49,9 @@ languages = reformatter(nation_data, "#Languages")
     data_array
   end
 
+end
 
-<tbody>
-															<tr class="nm-cnt01 nm-odd">
-																<td>Sunni Muslim</td>
-																<td>80.00</td>
-																<td>%</td>
-															</tr>
-															<tr class="nm-cnt02 nm-even">
-																<td>Shia Muslim</td>
-																<td>19.00</td>
-																<td>%</td>
-															</tr>
-															<tr class="nm-cnt03 nm-odd">
-																<td>other</td>
-																<td>1.00</td>
-																<td>%</td>
-															</tr>
-														</tbody>
+
+# afgan = Nokogiri::HTML(open('http://www.geognos.com/geo/en/countries-list/Country-Codes-ISO-3166-ISO-Numeric-ISO3-FIPS-ccTLD.html')).css("tbody tr")[0].css("a")[1]
+
+# afgan_data = Nokogiri::HTML(open("http://www.geognos.com/geo/en/cc/af.html"))
