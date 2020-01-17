fc3b479fae39139120db28c8f1b81c9787059335
Scott Hoisington <hoyzington@gmail.com>
2020-01-17 04:26:24 +0000

    scrape of nation list page

--- lib/nation_info/scraper.rb
+++ lib/nation_info/scraper.rb
@@ -4,18 +4,24 @@ require 'open-uri'
 
 class Scraper
   
+  def self.get_nations
+    Nokogiri::HTML(open('http://www.geognos.com/geo/en/countries-list/Country-Codes-ISO-3166-ISO-Numeric-ISO3-FIPS-ccTLD.html')).css("tbody tr")
+  end
   
+  def self.get_nation(nation_url)
+    
+  end
+    
   
 end
 
-data = Nokogiri::HTML(open('http://www.geognos.com/geo/en/countries-list/Country-Codes-ISO-3166-ISO-Numeric-ISO3-FIPS-ccTLD.html'))
 
-list = data.css("tbody tr")
+
 nation = list[i].css("a")[1]
 
 name = nation.text
-puts name
 
-url = nation("href")
-puts url
+url = nation.attribute("href").value
 
+
+afgan = Nokogiri::HTML(open('http://www.geognos.com/geo/en/countries-list/Country-Codes-ISO-3166-ISO-Numeric-ISO3-FIPS-ccTLD.html')).css("tbody tr")[0].css("a")[1]
